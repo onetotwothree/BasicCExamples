d5c01d09ba5a14e32a6b8a3bcf7337d68c38c99b
onetotwothree <65130906+onetotwothree@users.noreply.github.com>
2022-10-14 23:52:16 -0400

    Create increasingtriplet.c

---
+++ mediumLeetCode/increasingtriplet.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool increasingTriplet(int* nums, int numsSize){
+    if(numsSize < 3){
+        goto skip;
+    }
+    bool ret = false;
+    int lowest = nums[0];
+    int lowestIndex = 0;
+    int middle = nums[1];
+    int middleIndex = 1;
+    int highest = nums[numsSize - 1];
+    int highestIndex = numsSize - 1;
+    
+    for(int i = 1; i < numsSize - 1; i++){
+        printf("%i %i %i\n", lowest, middle, highest);
+        if(lowest < middle && middle < highest){
+            ret = true;
+            break;
+        }
+        if(nums[i] < lowest){
+            lowest = nums[i];
+            middle = nums[i + 1];
+        }
+        if(highest < nums[numsSize - 1 - i]){
+            highest = nums[numsSize - 1 - i];
+        }
+        if(nums[i + 1] > lowest && nums[i + 1] < highest){
+            middle = nums[i + 1];
+        }
+    }
+    printf("%i %i %i\n", lowest, middle, highest);
+    skip:
+    return ret;
+}
+
+int main(){
+    int nums[] = {2,1,0,2,0,2,0,2,0,2,0,1};
+    int numsSize = 12;
+    printf("%d\n",increasingTriplet(nums,numsSize));
+    return 0;
+}
